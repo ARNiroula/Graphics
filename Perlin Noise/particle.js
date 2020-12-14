9190c8f960f3ee656cd05cdb44bdcbafc4ec0421
ARNiroula <40847587+ARNiroula@users.noreply.github.com>
2020-12-14 21:27:29 +0545

    Field Behaviour Sliders added

--- Perlin Noise/particle.js
+++ Perlin Noise/particle.js
@@ -1,59 +1,60 @@
+class Particle {
+    constructor() {
+        this.pos = createVector(random(width), random(height))
+        this.vel = createVector(0, 0)
+        this.acc = createVector(0, 0)
+        this.maxspeed = sliderSpeed.value()
+        this.prevPos = this.pos.copy()
+    }
 
-function Particle(){
-    this.pos=createVector(random(width),random(height))
-    this.vel=createVector(0,0)
-    this.acc=createVector(0,0)
-    this.maxspeed=5
-    this.prevPos=this.pos.copy()
-
-    this.update=()=>{
+    update() {
         this.vel.add(this.acc)
         this.vel.limit(this.maxspeed)
         this.pos.add(this.vel)
         this.acc.mult(0)
+        this.maxspeed = sliderSpeed.value()
     }
 
-    this.applyforce=(force)=>{
+    follow(vectors) {
+        let x = floor(this.pos.x / scale)
+        let y = floor(this.pos.y / scale)
+        let index = x + y * cols
+        let force = vectors[index]
+        this.applyForce(force)
+    }
+
+    applyForce(force) {
         this.acc.add(force)
     }
-    this.show =()=>{
-        stroke(0,5)
+
+    show() {
+        stroke(sliderR.value(), sliderG.value(), sliderB.value(), 5)
         strokeWeight(1)
-        //point(this.pos.x,this.pos.y)
-        line(this.pos.x,this.pos.y,this.prevPos.x,this.prevPos.y)
-        this.updatePos()
+        line(this.pos.x, this.pos.y, this.prevPos.x, this.prevPos.y)
+        this.updatePrev()
     }
 
-    this.follow=(vectors)=>{
-        let x=floor(this.pos.x/scl)
-        let y=floor(this.pos.y/scl)
-        let index=x+y*cols
-        let force=p5.Vector.fromAngle(vectors[index])
-        force.setMag(1)
-        this.applyforce(force)
+    updatePrev() {
+        this.prevPos.x = this.pos.x
+        this.prevPos.y = this.pos.y
     }
 
-    this.updatePos=()=>{
-        this.prevPos.x=this.pos.x
-        this.prevPos.y=this.pos.y
-    }
-
-    this.edges =()=>{
-        if (this.pos.x > width ){
-            this.pos.x=0
-            this.updatePos()
+    edges() {
+        if (this.pos.x > width) {
+            this.pos.x = 0
+            this.updatePrev()
         }
-        if (this.pos.x < 0 ){
-            this.pos.x=width
-            this.updatePos()
+        if (this.pos.x < 0) {
+            this.pos.x = width
+            this.updatePrev()
         }
-        if (this.pos.y > height ){
-            this.pos.y=0
-            this.updatePos()
+        if (this.pos.y > height) {
+            this.pos.y = 0
+            this.updatePrev()
         }
-        if (this.pos.y < 0 ){
-            this.pos.y=height
-            this.updatePos()
+        if (this.pos.y < 0) {
+            this.pos.y = height
+            this.updatePrev()
         }
     }
 }
--- Perlin Noise/perlin_noise.js
+++ Perlin Noise/perlin_noise.js
@@ -1,60 +1,126 @@
 
-let inc=0.06
-let scl =10
-let cols,rows
-let fr
-let zoff=0
-let particle=[]
-let flowfield=[]
+let cols
+let rows
+let scale = 10
 
-    function setup() {
-    let cnv = createCanvas(600, 600);
+let increment = 0.1
+let currentFrameRate
+let zOffset = 0
+let particles = []
+let flowfield
 
-    cnv.position(10, 100);
-    pixelDensity(1)
-    cols=floor(width/scl)
-    rows=floor(height/scl)
-    fr=createP('')
-    //flowfield=new Array(col * rows)
-    for( let i =0; i<800;i++){
-        particle[i]= new Particle
-    }
+let sliderR
+let sliderG
+let sliderB
+let sliderFlowFieldSpeed
+let sliderMag
+let sliderSpeed
+
+let flowfieldVisible = false
+
+
+
+function setup() {
+	let canvas = createCanvas(800, 600)
+	canvas.parent('sketch-holder')
+	if (!flowfieldVisible) {
+		background(220)
+	}
+
+    //Sliders to change the behaviour of the flow field
+    //createP('Red        Green          Blue').parent('controls')
+    sliderR = createSlider(0, 255, 0, 1).parent('controls')
+    //createP('Green').parent('controls')
+    sliderG = createSlider(0, 255, 0, 1).parent('controls')
+    //createP('Blue').parent('controls')
+	sliderB = createSlider(0, 255, 0, 1).parent('controls')
+	createP('Speed of flowfield change').parent('controls')
+	sliderFlowFieldSpeed = createSlider(0.00005, 0.005, 0.0005, 0.00001).parent('controls')
+	createP('Magnitude of flowfield vector:').parent('controls')
+	sliderMag = createSlider(0.1, 3, 0.7, 0.1).parent('controls')
+	createP('Speed of the particles').parent('controls')
+	sliderSpeed = createSlider(0.1, 4, 2, 0.1).parent('controls')
+	createP('Framerate:').parent('controls')
+	currentFrameRate = createP('').parent('controls')
+	createP('Press \'s\' to save the image').parent('controls')
+
+	cols = floor(width / scale)
+	rows = floor(height / scale)
+	flowfield = new Array(cols * rows)
+
+	for (let i = 0; i < 1500; i++) {
+		particles[i] = new Particle()
+	}
+
+	//Toggle between particles and flowfield when button is clicked
+	document.querySelector(".flowfield-button").onclick = () => {
+		restartSketch()
+        flowfieldVisible = flowfieldVisible ? false : true
+        console.log(flowfieldVisible)
+	}
+
+	//Restart the sketch
+	document.querySelector(".clear-canvas").onclick = () => {
+		restartSketch()
+	}
+
+}
+
+
+function restartSketch() {
+	clear()
     background(220)
-    }
+	particles = []
+	for (let i = 0; i < 1500; i++) {
+		particles[i] = new Particle()
+	}
+}
 
-    function draw() {
-    let xoff=0
-    let yoff=0
-    
-    strokeWeight(1)
-    noFill()
-    stroke(0,50)
-    for (let y=0;y<height;y++){
-        xoff=0
-        y_pos=y*scl
-        for (let x=0;x<width;x++){
-            let index=x+y*cols
-            x_pos=x*scl    
-            let angle =noise(xoff,yoff,zoff)*TWO_PI *3
-            xoff+= inc 
-            //let  vec=p5.Vector.fromAngle(angle)
-            //vec.setMag(1)
-            flowfield[index]=angle
-            // push()
-            // translate(x_pos,y_pos)
-            // rotate(vec.heading())
-            // line(0,0,scl,0)
-            // pop()
-            }   
-        yoff +=inc
-        zoff += .01
-    }   
-    strokeWeight(3)
-    for( let i=0;i<particle.length;i++){    
-        particle[i].follow(flowfield)
-        particle[i].update()
-        particle[i].edges()
-        particle[i].show()
-    }
-    fr.html(floor(frameRate()))
+
+function draw() {
+	if (flowfieldVisible) {
+		background(220)
+	}
+	let yoff = 0
+	for (let y = 0; y < rows; y++) {
+		let xoff = 0
+		for (let x = 0; x < cols; x++) {
+			let index = x + y * cols
+			let angle = noise(xoff, yoff, zOffset) * TWO_PI * 4
+			let vec = p5.Vector.fromAngle(angle)
+			vec.setMag(sliderMag.value())
+			flowfield[index] = vec
+			xoff += increment
+
+			//draw the flowfield depending on whether the button has been clicked
+			if (flowfieldVisible) {
+                stroke(0, 50)
+                strokeWeight(2)
+				push()
+				translate(x * scale, y * scale)
+				rotate(vec.heading())
+				strokeWeight(1)
+				line(0, 0, scale, 0)
+				pop()
+			}
+		}
+		yoff += increment
+
+		zOffset += sliderFlowFieldSpeed.value()
+	}
+
+	for (let particle of particles) {
+		particle.follow(flowfield)
+		particle.update()
+		particle.edges()
+		particle.show()
+	}
+
+	currentFrameRate.html(floor(frameRate()))
+}
+
+
+
+keyReleased = function () {
+	if (key == 's' || key == 'S') save('myCanvas.jpg')
 }
